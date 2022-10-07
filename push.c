641d62b1b787d6541055f96ce3fd893214400ad1
LoloDum <chinedumokamgba@gmail.com>
2022-10-07 02:20:17 -0700

    push.c

--- push.c
+++ push.c
@@ -1,38 +1,54 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
 /**
- * push - push element into the stack
- * @stack: stack given by main
- * @line_cnt: amount of lines
+ * push - pushes a node to the top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
  *
- * Return: void
+ * Return: Nothing.
  */
-void push(stack_t **stack, unsigned int line_cnt)
+void push(stack_t **stack, unsigned int nline)
 {
-	char *n = global.argument;
+	stack_t *new;
 
-	if ((is_digit(n)) == 0)
+	if (stack == NULL)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+		fprintf(stderr, "L%d: stack not found\n", nline);
 		exit(EXIT_FAILURE);
 	}
 
-	if (global.data_struct == 1)
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
-		if (!add_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
+		exit(EXIT_FAILURE);
 	}
-	else
+
+	new->next = *stack;
+	new->prev = NULL;
+	new->n = arg.arg;
+	if (*stack)
+		(*stack)->prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ *
+ * Return: Nothing.
+ */
+void pall(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+	(void)nline;
+
+	temp = *stack;
+	while (temp)
 	{
-		if (!queue_node(stack, atoi(global.argument)))
-		{
-			exit(EXIT_FAILURE);
-		}
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
